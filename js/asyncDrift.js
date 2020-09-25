ddb869eaaae3b36ce295b4b1bb8937c378b6655c
Miyayes <cnguyen@brave.com>
2020-09-25 15:01:58 -0400

    Add mutation observer

--- js/asyncDrift.js
+++ js/asyncDrift.js
@@ -12,17 +12,37 @@ const driftIconStyles = {
 Object.assign(driftIcon.style, driftIconStyles);
 console.log("Created chatbot icon image.");
 driftIcon.onclick = loadDriftBot;
+
 function loadDriftBot() {
   driftIcon.style.display = "none";
   let drift = document.createElement("script");
   drift.src = "js/driftt-chatbot.js";
   document.body.appendChild(drift);
-  console.log("Set driftbot as script src.");
-  console.log("Trying to click the #drift-widget...");
-  drift.onload = clickDriftBot;
+  console.log("Created <script> w/ src = external script.");
 }
 
 function clickDriftBot() {
   document.getElementById('widgetButton').click(); //drift-widget created by Drift's external script
   console.log("clicked drift bot");
 }
+
+let observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (!mutation.addedNodes) return
+
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      if (document.getElementById('widgetButton')) {
+        clickDriftBot();
+        observer.disconnect()
+      }
+      let node = mutation.addedNodes[i]
+    }
+  })
+})
+
+observer.observe(document.body, {
+    childList: true
+  , subtree: true
+  , attributes: false
+  , characterData: false
+})
