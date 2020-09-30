bc1ca821a15aa6e9e63308be33e1a466acc41c9f
Miyayes <cnguyen@brave.com>
2020-09-30 13:50:34 -0400

    Create custom chat icon

--- js/asyncDrift.js
+++ js/asyncDrift.js
@@ -1,5 +1,6 @@
 let driftIcon = document.createElement("img");
 driftIcon.src = "images/drift-bot-icon.png";
+driftIcon.id = "driftIcon";
 document.body.appendChild(driftIcon);
 const driftIconStyles = {
   position: 'fixed',
@@ -7,42 +8,48 @@ const driftIconStyles = {
   right: '15px',
   zIndex: '10',
   cursor: 'pointer',
-  maxWidth: '375px'
+  maxWidth: '300px'
 };
 Object.assign(driftIcon.style, driftIconStyles);
-console.log("Created chatbot icon image.");
 driftIcon.onclick = loadDriftBot;
 
 function loadDriftBot() {
   driftIcon.style.display = "none";
+  closeChat.style.display = "none";
   let drift = document.createElement("script");
   drift.src = "js/driftt-chatbot.js";
   document.body.appendChild(drift);
-  console.log("Created <script> w/ src = external script.");
 }
 
-function clickDriftBot() {
-  document.getElementById('widgetButton').click(); //drift-widget created by Drift's external script
-  console.log("clicked drift bot");
+let closeChat = document.createElement("img");
+closeChat.src = "images/xbutton.svg";
+closeChat.id = "closeChat";
+document.body.appendChild(closeChat);
+const closeChatStyles = {
+  position: 'fixed',
+  bottom: '80px',
+  right: '10px',
+  zIndex: '10',
+  cursor: 'pointer',
+  maxWidth: '10px'
+};
+Object.assign(closeChat.style, closeChatStyles);
+closeChat.onclick = closePrompt;
+
+function closePrompt() {
+  closeChat.style.display = "none";
+  driftIcon.style.display = "none";
 }
 
-let observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (!mutation.addedNodes) return
-
-    for (let i = 0; i < mutation.addedNodes.length; i++) {
-      if (document.getElementById('widgetButton')) {
-        clickDriftBot();
-        observer.disconnect()
-      }
-      let node = mutation.addedNodes[i]
-    }
-  })
-})
-
-observer.observe(document.body, {
-    childList: true
-  , subtree: true
-  , attributes: false
-  , characterData: false
-})
+let chatStyles = document.createElement("style");
+chatStyles.innerHTML = `
+#closeChat:hover {
+    opacity: 75%;
+    transform: scale(1.1);
+    transition: 0.15s;
+  }
+#driftIcon:hover {
+    transform: scale(1.02);
+    transition: 0.25s;
+}`;
+document.body.appendChild(chatStyles);
